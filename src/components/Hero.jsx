eb307ded5a41fe4176699dd6eafe72212c3b3e72
Neha Rai <neharadialcode@gmail.com>
2024-06-12 13:44:17 +0530

    validation added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,12 +6,23 @@ const Hero = () => {
   const {
     register,
     handleSubmit,
-    watch,
+    reset,
     formState: { errors },
-  } = useForm();
+  } = useForm({
+    defaultValues: {
+      name: "",
+      email: "",
+      course: "",
+      fees: null,
+    },
+  });
 
   const onSubmit = (data) => {
-    console.log(data, "data");
+    try {
+      reset();
+    } catch (error) {
+      console.log(error, "error");
+    }
   };
 
   return (
@@ -24,27 +35,31 @@ const Hero = () => {
         className="max-w-[700px] mx-auto "
       >
         <input
-          className="border-blue-400 bg-transparent border w-full my-5inline-block p-5 mt-4"
+          className="border-blue-400 bg-transparent border w-full my-5inline-block p-5 mt-4 mb-2"
           placeholder="Name"
-          {...register("name")}
+          {...register("name", { required: "Name is required" })}
         />
+        {errors.name && <p role="alert">{errors.name.message}</p>}
         <input
           placeholder="Email"
-          className="border-blue-400 bg-transparent border w-full my-5inline-block p-5 mt-4"
-          {...register("email", { required: true })}
+          type="email"
+          className="border-blue-400 bg-transparent border w-full my-5inline-block p-5 mt-4 mb-2"
+          {...register("email", { required: "Email Address is required" })}
         />
+        {errors.email && <p role="alert">{errors.email.message}</p>}
         <input
           placeholder="Course"
-          className="border-blue-400 bg-transparent border w-full my-5inline-block p-5 mt-4"
-          {...register("course", { required: true })}
+          className="border-blue-400 bg-transparent border w-full my-5inline-block p-5 mt-4 mb-2"
+          {...register("course", { required: "Course is required!!" })}
         />
+        {errors.course && <p role="alert">{errors.course.message}</p>}
         <input
           placeholder="Fees"
-          className="border-blue-400 bg-transparent border w-full my-5inline-block p-5 mt-4"
+          type="number"
+          className="border-blue-400 bg-transparent border w-full my-5inline-block p-5 mt-4 mb-2"
           {...register("fees", { required: true })}
         />
-        {errors.exampleRequired && <span>This field is required</span>}
-
+        {errors.fees && <span>This field is required</span>}
         <div className="text-center">
           <button
             type="submit"
